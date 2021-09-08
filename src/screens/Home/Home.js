2c8ed4706ece4c108de566b69b8e1bdd5a865043
Kirill2034 <dum15.00@mail.ru>
2021-09-08 20:10:32 +0400

    Created a page Home

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.Home}>
+            <div className={styles.HomeNav}>
+                <div className={styles.Logo}>
+                    <p>Thrivetalk</p>
+                </div>
+                <div className={styles.Navbar}>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Services</p>
+                    <p>Blog</p>
+                </div>
+                <div className={styles.CountactBtn}>
+                    <p>Contuct us</p>
+                </div>
+            </div>
+            <div className={styles.HomeContent}>
+                <div className={styles.HomeContentText}>
+                    <div className={styles.HomeContentTextLogo}>
+                        <p>Thivetalk</p>
+                    </div>
+                    <div className={styles.HomeContentTextUpperText}>
+                        <p>HELPING YOU THRIVE IN ALL AREAS OF LIFE</p>
+                    </div>
+                    <div className={styles.HomeContentTextPar}>
+                        <p>Our highly talented therapists can help you with a range of issues including relationships, sex, PTSD, depression, social anxiety, or even just caring for yourself more.</p>
+                    </div>
+                    <div className={styles.HomeContentTextBtn}>
+                        <p>Who am i</p>
+                        <p>What do i do</p>
+                    </div>
+                </div>
+                <div className={styles.HomeContentImage}>
+                    <img src='./img/Rectangle.png' alt='text'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
